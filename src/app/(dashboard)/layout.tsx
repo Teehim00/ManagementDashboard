73ac5db0bb5972ea3faccda69116b7780ade62e0
Amornthep <amornthep.tee@gmail.com>
2025-01-29 22:49:46 +0700

    feat: redesign admin UI layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,8 @@
+import Menu from "../components/Menu";
+import Image from "next/image";
+import Link from "next/link";
+import Navbar from "../components/Navbar";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -6,9 +11,21 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] bg-red-200">L</div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-pink-50 p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">Dashboard</span>
+        </Link>
+        <Menu />
+      </div>
       {/* RIGHT */}
-      <div className="w-[86%] bg-blue-200">R</div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-50 overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +8,21 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        LamaSky: "#C3EBFA",
+        LamaSkyLight: "#EDF9FD",
+        LamaPurple: "#CFCEFF",
+        LamaPurpleLight: "#F1F0FF",
+        LamaYellow: "#FAE27C",
+        LamaYellowLight: "#FAE27C",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
